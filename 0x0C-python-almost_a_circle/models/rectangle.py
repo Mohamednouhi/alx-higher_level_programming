4ce429c7bd5ec2ed67ee7f6e4cad97cbdd791508
mohamed nouhi <mohamednoouuhi@gmail.com>
2023-11-17 03:14:25 -0800

    all

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -110,4 +110,14 @@ class Rectangle(Base):
     def to_dictionary(self):
         """Converts the rectangle to a dictionary."""
         return {"id": self.id, "width": self.__width, "height": self.__height, "x": self.__x, "y": self.__y}
+if __name__ == "__main__":
+
+    r1 = Rectangle(10, 2)
+    print(r1.id)
+
+    r2 = Rectangle(2, 10)
+    print(r2.id)
+
+    r3 = Rectangle(10, 2, 0, 0, 12)
+    print(r3.id)
 

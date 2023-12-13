b5a6f76f3760f388233a78da0e0bd758babb5343
mohamed nouhi <you@example.com>
2023-12-13 15:29:32 -0800

    create and grant

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,4 @@
---create database if it doesnt exists
-CREATE IF NOT EXISTS hbtn_0d_2;
---create user if it doesnt exist
+--create grant if it doesnt exists
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
---grant select privilige to the user
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';

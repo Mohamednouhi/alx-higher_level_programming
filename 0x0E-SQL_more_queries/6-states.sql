13e209c2707fe9400a631b81dc9e23680f3cc87d
mohamed nouhi <you@example.com>
2023-12-13 15:51:33 -0800

    not null primary unique

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,9 +1,12 @@
 -- Create database hbtn_0d_usa if it does not exist
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
 -- Switch to the hbtn_0d_usa database
 USE hbtn_0d_usa;
---à Create table states if it does not exist
+
+-- Create table states if it does not exist
 CREATE TABLE IF NOT EXISTS states (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(256) NOT NULL
 );
+

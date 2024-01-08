199b90c66155da74c4055bd321bf64668a24c603
mohamed nouhi <mohamednoouuhi@gmail.com>
2024-01-09 00:57:06 +0100

    arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,13 +1,4 @@
 #!/usr/bin/node
 
 const arguments = process.argv.length - 2;
-
-if ( arguments == 0 ) {
-	console.log('No argument');
-} 
-else if ( arguments == 1 ) {
-	console.log('Argument found');
-} 
-else {
-	console.log('Arguments found');
-}
+console.log(arguments === 0 ? 'No argument' : arguments === 1 ? 'Argument found' : 'Arguments found');

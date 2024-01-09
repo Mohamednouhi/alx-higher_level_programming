88e3a152b54574f9d5bd48b535c64eea13df01fe
mohamed nouhi <mohamednoouuhi@gmail.com>
2024-01-09 12:55:05 +0100

    if

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
-const args = process.argv[2];
-const num = parseInt(args);
-if (!isNaN(num) && num>0) {
-	for ( let i = 0; i<num; i++) {
-		console.log("C is fun");
+const num = Math.floor(Number(process.argv[2]));
+if (!isNaN(num)) {
+	for (let i=0; i<num; i++) {
+		console.log('C is fun');
 	}
 } else {
-	console.log("Missing number of occurrences");
+	console.log('Missing number of occurrences');
 }

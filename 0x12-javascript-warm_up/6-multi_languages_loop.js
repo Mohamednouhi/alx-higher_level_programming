609c42a24398350cd5995f987e4c77d59f6c31ed
mohamed nouhi <mohamednoouuhi@gmail.com>
2024-01-09 12:18:18 +0100

    for

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
 
-const myVar = ["C is fun", "Python is cool", "JavaScript is amazing"];
-for (let i=0; i<myVar.length; i++) {
-	console.log(myVar[i]);
+for (const languages of ["C is fun", "Python is cool", "JavaScript is amazing"]) {
+	console.log(languages);
 }

23668b6591e5df1d45b17193ecf601056fc368eb
mohamed nouhi <mohamednoouuhi@gmail.com>
2024-01-11 00:42:06 +0100

    -

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,15 +1,10 @@
 #!/usr/bin/node
-module.exports = class Square extends require ('./5-square?js') {
-	super();
-	charPrint(c) {
-		if (c!== undefined) {
-			for(let i =0; i<this.heigth; i++) {
-				for (let j=0; j<this.width; j++) {
-					console.log('X');
-				}
-			}
-		} else {
-			this.print();
-		}
-	}
+module.exports = class Square extends require('./5-square.js') {
+  charPrint (c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) console.log(c.repeat(this.width));
+    }
+  }
 };

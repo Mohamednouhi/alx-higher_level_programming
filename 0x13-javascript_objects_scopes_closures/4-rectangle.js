ca6aaed096a0e3be1aa1d3c641febc63a0088376
mohamed nouhi <mohamednoouuhi@gmail.com>
2024-01-11 00:08:18 +0100

    []

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,31 +1,18 @@
 #!/usr/bin/node
-
 module.exports = class Rectangle {
-  constructor(w, h) {
-    if (w > 0 && h > 0) {
-      this.width = w;
-      this.height = h;
-    }
+  constructor (w, h) {
+    if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
   }
 
-  print() {
-    if (this.width && this.height) {
-      for (let i = 0; i < this.height; i++) {
-        for (let j = 0; j < this.width; j++) {
-          console.log('X');
-        }
-      }
-    }
+  print () {
+    for (let i = 0; i < this.height; i++) console.log('X'.repeat(this.width));
   }
 
-  rotate() {
-    if (this.width && this.height) {
-      [this.width, this.height] = [this.height, this.width];
-    }
+  rotate () {
+    [this.width, this.height] = [this.height, this.width];
   }
 
-  double() {
+  double () {
     [this.width, this.height] = [this.width * 2, this.height * 2];
   }
-}
-
+};
